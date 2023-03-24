b4e32efd5fa9b9e758e9854e138b5de68c7fa971
Eduardo Antonio Pérez <eduardoperez@gmail.com>
2023-03-23 21:10:39 -0600

    Commit Examen II Parcial

---
+++ Tickets/Vista/Login.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Vista
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.ContraseñaTextBox = new System.Windows.Forms.TextBox();
+            this.AccederButton = new System.Windows.Forms.Button();
+            this.SalirButton = new System.Windows.Forms.Button();
+            this.MostrarContraseñaButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(310, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(59)))), ((int)(((byte)(104)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(177, 279);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Vista.Properties.Resources.Login;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(223, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(223, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuario";
+            // 
+            // UsuarioTextBox
+            // 
+            this.UsuarioTextBox.Location = new System.Drawing.Point(226, 136);
+            this.UsuarioTextBox.Name = "UsuarioTextBox";
+            this.UsuarioTextBox.Size = new System.Drawing.Size(214, 22);
+            this.UsuarioTextBox.TabIndex = 5;
+            // 
+            // ContraseñaTextBox
+            // 
+            this.ContraseñaTextBox.Location = new System.Drawing.Point(226, 190);
+            this.ContraseñaTextBox.Name = "ContraseñaTextBox";
+            this.ContraseñaTextBox.PasswordChar = '*';
+            this.ContraseñaTextBox.Size = new System.Drawing.Size(214, 22);
+            this.ContraseñaTextBox.TabIndex = 6;
+            // 
+            // AccederButton
+            // 
+            this.AccederButton.BackColor = System.Drawing.Color.White;
+            this.AccederButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AccederButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AccederButton.Location = new System.Drawing.Point(286, 232);
+            this.AccederButton.Name = "AccederButton";
+            this.AccederButton.Size = new System.Drawing.Size(85, 27);
+            this.AccederButton.TabIndex = 9;
+            this.AccederButton.Text = "Acceder";
+            this.AccederButton.UseVisualStyleBackColor = false;
+            this.AccederButton.Click += new System.EventHandler(this.AccederButton_Click);
+            // 
+            // SalirButton
+            // 
+            this.SalirButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.SalirButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SalirButton.Image = global::Vista.Properties.Resources.boton_eliminar;
+            this.SalirButton.Location = new System.Drawing.Point(461, 0);
+            this.SalirButton.Name = "SalirButton";
+            this.SalirButton.Size = new System.Drawing.Size(38, 35);
+            this.SalirButton.TabIndex = 10;
+            this.SalirButton.UseVisualStyleBackColor = true;
+            this.SalirButton.Click += new System.EventHandler(this.SalirButton_Click);
+            // 
+            // MostrarContraseñaButton
+            // 
+            this.MostrarContraseñaButton.Image = global::Vista.Properties.Resources.ver;
+            this.MostrarContraseñaButton.Location = new System.Drawing.Point(412, 214);
+            this.MostrarContraseñaButton.Name = "MostrarContraseñaButton";
+            this.MostrarContraseñaButton.Size = new System.Drawing.Size(28, 23);
+            this.MostrarContraseñaButton.TabIndex = 8;
+            this.MostrarContraseñaButton.UseVisualStyleBackColor = true;
+            this.MostrarContraseñaButton.Click += new System.EventHandler(this.MostrarContraseñaButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Vista.Properties.Resources.user;
+            this.pictureBox2.Location = new System.Drawing.Point(302, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 72);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.AccederButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CancelButton = this.SalirButton;
+            this.ClientSize = new System.Drawing.Size(496, 279);
+            this.ControlBox = false;
+            this.Controls.Add(this.SalirButton);
+            this.Controls.Add(this.AccederButton);
+            this.Controls.Add(this.MostrarContraseñaButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.ContraseñaTextBox);
+            this.Controls.Add(this.UsuarioTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox UsuarioTextBox;
+        private System.Windows.Forms.TextBox ContraseñaTextBox;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button MostrarContraseñaButton;
+        private System.Windows.Forms.Button AccederButton;
+        private System.Windows.Forms.Button SalirButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Tickets/Vista/Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void AccederButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(UsuarioTextBox.Text))
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese un Usuario");
+                UsuarioTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            if (string.IsNullOrEmpty(ContraseñaTextBox.Text))
+            {
+                errorProvider1.SetError(ContraseñaTextBox, "Ingrese una Contraseña");
+                ContraseñaTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+            Menu menuFormulario = new Menu();
+            Hide();
+            menuFormulario.Show();
+
+        }
+
+        private void MostrarContraseñaButton_Click(object sender, EventArgs e)
+        {
+            if (ContraseñaTextBox.PasswordChar == '*')
+            {
+                ContraseñaTextBox.PasswordChar = '\0';
+            }
+            else
+            {
+                ContraseñaTextBox.PasswordChar = '*';
+            }
+        }
+
+        private void SalirButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
